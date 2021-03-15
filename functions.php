47cf3914aaefba4c49f90ce2b1dbef663fde42c7
Johan van der Wijk <johan@vanderwijk.nl>
2021-03-15 21:25:07 +0100

    Tag manager

--- functions.php
+++ functions.php
@@ -12,6 +12,14 @@ require 'shortcode-calculator.php';
 
 function bestelgrindenzand_tracking() { ?>
 
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NKLR5NP');</script>
+<!-- End Google Tag Manager -->
+
 <!-- Global site tag (gtag.js) - Google Ads: 666638924 -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=AW-666638924"></script>
 <script>
@@ -39,6 +47,16 @@ function bestelgrindenzand_tracking() { ?>
 <?php }
 }
 add_action( 'wp_head', 'bestelgrindenzand_tracking', 10 );
+
+function bestelgrindenzand_scripts_body() { ?>
+
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NKLR5NP"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
+<?php }
+add_action( 'wp_body_open', 'bestelgrindenzand_scripts_body', 10 );
 
 
 // Get most expensive variable and tag this for remarketing in Google Ads
