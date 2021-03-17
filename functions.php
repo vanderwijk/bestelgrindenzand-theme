1fc7e8180b2a7621607df0321da84fb64c37be19
Johan van der Wijk <johan@vanderwijk.nl>
2021-03-17 13:52:44 +0100

    Hotjar script verwijderd

--- functions.php
+++ functions.php
@@ -10,7 +10,7 @@ add_action( 'after_setup_theme', 'bestelgrindenzand_child_theme_setup' );
 require 'shortcode-calculator.php';
 
 
-function bestelgrindenzand_tracking() { ?>
+function bestelgrindenzand_scripts_head() { ?>
 
 <!-- Google Tag Manager -->
 <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
@@ -30,23 +30,8 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 	gtag('config', 'AW-666638924');
 </script>
 
-<?php if ( !is_user_logged_in() ) {  // HotJar only for logged out users ?>
-
-<!-- Hotjar Tracking Code for https://bestelgrindenzand.nl -->
-<script>
-(function(h,o,t,j,a,r){
-	h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-	h._hjSettings={hjid:2262080,hjsv:6};
-	a=o.getElementsByTagName('head')[0];
-	r=o.createElement('script');r.async=1;
-	r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-	a.appendChild(r);
-})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-</script>
-
 <?php }
-}
-add_action( 'wp_head', 'bestelgrindenzand_tracking', 10 );
+add_action( 'wp_head', 'bestelgrindenzand_scripts_head', 10 );
 
 function bestelgrindenzand_scripts_body() { ?>
 
