a29106adb271515b01fa7f21bea735f2135ff8e2
Johan van der Wijk <johan@vanderwijk.nl>
2023-05-08 11:34:49 +0200

    Widget styling

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 
-define('BESTELGRINDENZAND_THEME_VER', '1.2.6');
+define('BESTELGRINDENZAND_THEME_VER', '1.2.7');
 
 // Child theme textdomain
 function bestelgrindenzand_child_theme_setup() {
